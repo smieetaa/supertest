dcc8aab1961d8aecc53aceff5a119918e95324f2
DESKTOP-7DS3AJF\nares <smieetaa@gmail.com>
2023-02-15 14:59:23 +1100

    api testing using supertest chai mocha

---
+++ ReadMe.md
@@ -0,0 +1,40 @@
+# API Testing using SuperTest, Chai and Mocha
+
+## API Automation test assignment
+1. API 1: Get All Products List: https://automationexercise.com/api/productsList
+        1. Use above endpoint to GET a JSON response and assert that 200 OK is received
+        2. Assert that 1st node in products JSON array returns “blue top”
+
+2. API 11: POST To Create/Register User Account: https://automationexercise.com/api/createAccount
+        1. Use above endpoint to trigger a POST method
+        2. assert the http response code is 200 
+        3. assert that “responseCode” in the response body is 201 
+        4. assert that “message” in the response body is “User created!”
+
+## Directory Structure:
+
+supertest-chai-mocha
+        - api-tests: the API test file
+        - configs: environment configuration parameters likes URL
+        - sample-exec-result: sample result screenshot
+        - test-data: api test data  
+        - package.json: dependencies
+
+## Getting Started:
+
+You need to have the following installed in your machine:
+
+1. Install node and npm - https://nodejs.org/en/download/
+2. verify the installation using below command:
+   `node -v`
+   `npm -v`
+3. install supertest:
+   `npm install supertest --sasave-dev` 
+4. `mocha` framework is used for writing tests and `chai` for assertions, following command should help to install the required npm packages:
+   `npm i -D -g supertest mocha chai mochawesome`
+5. to run the test, run the command: `npm run test`
+6. to generate the mochawesome report, run the command `npm run test-report`. You can view the report in `mochawesome.html` file in the `mochawesome-report` folder..
+
+<img src="sample-exec-result/test.png"/>
+
+<img src="sample-exec-result/test-report.png"/>
---
+++ api/apiExercise.js
@@ -0,0 +1,54 @@
+const request = require('supertest');
+const assert = require('chai').assert;
+const config = require('../config/config.json');
+const userProfile = require('../test-data/user.json');
+
+describe('API testing using SuperTest - Chai - Mocha', () => {
+
+  it('1. Get All Products List (API 1): verify first product name', async () => {
+    return request(config.baseUrl)
+      .get(config.products.getProducts)
+      .then(res => {
+        // assert that 200 OK is received 
+        assert.equal(res.statusCode, 200, "status code is not 200");
+
+        // verifu products list exists
+        const resBody = JSON.parse(res.text);
+        assert.notEqual(resBody.products, null, "");
+        assert.isAtLeast(resBody.products.length, 1, "3 is greater or equal to 3");
+
+        // assert that 1st node in products JSON array returns “blue top”
+        assert.equal(resBody.products[0].name, "Blue Top", "first product name is not 'Blue Top");
+      });
+  });
+
+
+  it('2. POST To Create/Register User Account (API 11): verify user is created', async () => {
+    // delete existing user account
+    await request(config.baseUrl)
+      .delete(config.users.deleteAccount)
+      .type('form')
+      .send(userProfile)
+      .then(res => {
+        // assert that 200 OK is received 
+        assert.equal(res.statusCode, 200, "did not delete existing user - status code is not 200");
+      });
+
+    // create new user account
+    return request(config.baseUrl)
+      .post(config.users.createAccount)
+      .type('form')
+      .send(userProfile)
+      .then(res => {
+        // assert that 200 OK is received 
+        assert.equal(res.statusCode, 200, "status code is not 200");
+
+        // assert that “responseCode” in the response body is 201
+        const resBody = JSON.parse(res.text);
+        assert.equal(resBody.responseCode, 201, "responseCode is not 201");
+
+        // assert that “message” in the response body is “User created!”
+        assert.equal(resBody.message, "User created!", "res message is not 'User created!'");
+      });
+  });
+});
